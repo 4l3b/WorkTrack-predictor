a23905ee1d0f00eda75cc1c2febb2724904aa37c
4l3b <alessandro.bigolin.fdi@gmail.com>
2026-08-05 12:57:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ try:
     # Copyright
     st.markdown(
         """
-        <div style='text-align: center; margin-top: 4rem; font-size: 0.8rem; color: #777777;'>
+        <div style='text-align: center; margin-top: 2rem; font-size: 0.8rem; color: #777777;'>
             &copy; 2026 Alessandro Bigolin
         </div>
         """, 
